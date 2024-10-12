dc0cf6b75ab71104a8ebad01a768d5d96e01c0eb
Jerome Bell <jeromebell@gmail.com>
2024-10-12 11:01:11 -0500

    Added Application Settings

--- src/BlazorPages/Program.cs
+++ src/BlazorPages/Program.cs
@@ -1,11 +1,18 @@
+using System.Net.Http.Json;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorPages;
+using BlazorPages.Util;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var http = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+builder.Services.AddScoped(sp => http);
 
-await builder.Build().RunAsync();
+var appSettings = await http.GetFromJsonAsync<AppSettings>("appsettings.json", new System.Text.Json.JsonSerializerOptions() { Converters = { new UnknownEnumConverter() } });
+builder.Services.AddSingleton(appSettings ?? new());
+
+await builder.Build().RunAsync
+    ();
